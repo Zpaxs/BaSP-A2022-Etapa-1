eda4d4afa2a6a8db7ffd5f981c1d432727e70a06
Soledad Acosta <soledadacosta0@gmail.com>
2022-09-20 22:27:57 -0300

    BaSP-W05-2022: Edited excercise 04

--- Semana-05/exercises/04_if_else.js
+++ Semana-05/exercises/04_if_else.js
@@ -16,35 +16,35 @@ console.log("age: " + age);
 
 // i. “Bebe” si la edad es menor a 2 años;
 if (age <= 2) {
-  alert("Si la edad es: " + age + " es un Bebé");
+  alert("If the age is: " + age + " it's a baby");
 }
 
 // ii. “Niño” si la edad es entre 2 y 12 años;
 else if (age > 2 && age < 12) {
-  alert("Si la edad es: " + age + " es un Niño");
+  alert("If the age is: " + age + " it's a child");
 }
 
 // iii. “Adolescente” entre 13 y 19 años;
 else if (age > 13 && age < 19) {
-  alert("Si la edad es: " + age + " es un Adolescente");
+  alert("If the age is: " + age + " it's a teenager");
 }
 
 // iv. “Joven” entre 20 y 30 años;
 else if (age > 20 && age < 30) {
-  alert("Si la edad es: " + age + " es un Joven");
+  alert("If the age is: " + age + " it's a young person");
 }
 
 // v. “Adulto” entre 31 y 60 años;
 else if (age > 31 && age < 60) {
-  alert("Si la edad es: " + age + " es un Adulto");
+  alert("If the age is: " + age + " its an adult");
 }
 
 // vi. “Adulto mayor” entre 61 y 75 años;
 else if (age > 61 && age <= 75) {
-  alert("Si la edad es: " + age + " es un Adulto mayor");
+  alert("If the age is: " + age + " its an older person");
 }
 
 // vii. “Anciano” si es mayor a 75 años.
 else {
-  alert("Si la edad es: " + age + " es un Anciano");
+  alert("If the age is: " + age + " it's an elder");
 }
